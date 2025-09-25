029f1f62e8d36dbc3c139a10b94e4788005142e6
GreatGadBB <alexsheppard54@gmail.com>
2025-09-25 17:59:31 +0300

    idk

--- LW/L1/ScalarTypes.js
+++ LW/L1/ScalarTypes.js
@@ -1,13 +1,9 @@
-function inc(n){
-    return (1+(1/n))**n;
-};
+const inc = n => (1+(1/n))**n;
 const jeden=999;
 const dwa=inc(jeden);
 console.dir({jeden,dwa});
 
-function incTwo(a){
-    a.aaa=a.aaa/(-1);
-};
+function incTwo(a){ a.aaa=a.aaa/(-1);};
 const aa={ aaa:1 };
 incTwo(aa);
 console.dir(aa);
