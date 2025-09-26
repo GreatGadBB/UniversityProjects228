04f22388fcb3db5916983cc5cf8a5f2aa12b2c4f
GreatGadBB <alexsheppard54@gmail.com>
2025-09-26 19:53:31 +0300

    Changed folder name for future

---
+++ LW-1/L1/ScalarTypes.js
@@ -0,0 +1,9 @@
+const inc = n => (1+(1/n))**n;
+const jeden=999;
+const dwa=inc(jeden);
+console.dir({jeden,dwa});
+
+function incTwo(a){ a.aaa=a.aaa/(-1);};
+const aa={ aaa:1 };
+incTwo(aa);
+console.dir(aa);
