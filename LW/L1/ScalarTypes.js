484eb8b7ccde4f950b703b49dbe3952ec524d8be
GreatGadBB <alexsheppard54@gmail.com>
2025-09-26 19:55:33 +0300

    idk3

--- LW/L1/ScalarTypes.js
+++
@@ -1,9 +0,0 @@
-const inc = n => (1+(1/n))**n;
-const jeden=999;
-const dwa=inc(jeden);
-console.dir({jeden,dwa});
-
-function incTwo(a){ a.aaa=a.aaa/(-1);};
-const aa={ aaa:1 };
-incTwo(aa);
-console.dir(aa);
