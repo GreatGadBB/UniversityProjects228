c8c5a0b53058b9b6ac76f10c12c0d8b76ee905d1
GreatGadBB <alexsheppard54@gmail.com>
2025-10-12 13:48:35 +0300

    L2 done

--- LW-1/L2/Colections.js
+++ LW-1/L2/Colections.js
@@ -1,1 +1,19 @@
-const dox228 = [{}]
+const dox228 = [{name:"Donald Trump", phone:"your mom"},{name:"Joe Biden", phone:"your mama"},{name:"your mom",phone:"0279591"},{name:"me",phone:"0219999"}];
+const dox229 = {
+    "Donald Trump":"your mom",
+    "Joe Biden":"your mama",
+    "your mom":"0279591",
+    "me":"0129999"};
+const tasknum=9;
+const findPhoneByName=(name)=>{
+    if(tasknum===9){
+        for(const i of dox228){
+                if(i.name===name){
+                    return i.phone; //NOWAYING iphone
+                };
+            };
+    }else if(tasknum===10){
+        return dox229[name];
+    };
+} ;
+console.log(findPhoneByName("your mom"))
