e1ec61d6ff57915b30adc2caeb544ac2450db5e3
GreatGadBB <alexsheppard54@gmail.com>
2026-02-27 15:21:08 +0200

    yes

--- LW-1/L2/Functions.js
+++
@@ -1,8 +0,0 @@
-const average = (a,b) => (a+b)/2;
-const square = (x) => x*x;
-const cube = (x) => x*x*x;
-const mathBtw=[];
-for(let i=0; i<=9; i++){
-    mathBtw.push(average(square(i),cube(i)));
-};
-console.log(mathBtw);
