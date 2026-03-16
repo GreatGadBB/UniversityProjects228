19583801038ce40ca9fc3a0fb1d2bfac5962ed40
GreatGadBB <alexsheppard54@gmail.com>
2026-03-16 12:23:36 +0200

    unfixed the fix that broke everything apparently

--- project/lab3.js
+++ project/lab3.js
@@ -11,7 +11,7 @@ function memoy(mf, delCustom){
 
 
         for(const i in cache){if(Date.now()-cache[i].lru>10) delete cache[i];} //time deletion
-        if (cache.length>=maxCacheSize){
+        if (Object.keys(cache).length>=maxCacheSize){
             let least=Infinity;
             let list;
             if (typeof delCustom === "function"){
